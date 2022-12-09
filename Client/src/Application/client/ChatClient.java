8f56d53ca161b149077a66d37602e09fd6a7c3ac
audiblemaple <audiblemaple@gmail.com>
2022-12-09 20:12:50 +0200

    .

---
+++ Client/src/Application/client/ChatClient.java
@@ -0,0 +1,158 @@
+// This file contains material supporting section 3.7 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+
+package Application.client;
+
+import Application.Common.ChatIF;
+import Application.Common.Message;
+import Application.Common.MessageFromClient;
+import Application.Common.Subscriber;
+import ocsf.AbstractClient;
+
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * This class overrides some of the methods defined in the abstract
+ * superclass in order to give more functionality to the client.
+ *
+ * @author Dr Timothy C. Lethbridge
+ * @author Dr Robert Lagani&egrave;
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @version July 2000
+ */
+public class ChatClient extends AbstractClient
+{
+  //Instance variables **********************************************
+  
+  public static Subscriber s1 = new Subscriber(null,null,null,null,null,null,null);
+  public static ArrayList<Subscriber> subs = new ArrayList<>();
+  public static boolean awaitResponse = false;
+  public static String servermsg = new String();
+  /**
+   * The interface type variable.  It allows the implementation of
+   * the display method in the client.
+   */
+  ChatIF clientUI;
+
+  //Constructors ****************************************************
+  
+  /**
+   * Constructs an instance of the chat client.
+   *
+   * @param host The server to connect to.
+   * @param port The port number to connect on.
+   * @param clientUI The interface type variable.
+   */
+	 
+  public ChatClient(String host, int port, ChatIF clientUI) 
+    throws IOException 
+  {
+    super(host, port); //Call the superclass constructor
+    this.clientUI = clientUI;
+    //openConnection();
+  }
+
+  //Instance methods ************************************************
+    
+  /**
+   * This method handles all data that comes in from the server.
+   *
+   * @param msg The message from the server.
+   */
+  public void handleMessageFromServer(Object msg) 
+  {
+	  subs = new ArrayList<>();
+	  System.out.println("--> handleMessageFromServer");
+	  System.out.println(msg);
+     
+	  awaitResponse = false;
+	  String st;
+	  st=msg.toString();
+	  BufferedReader bufReader = new BufferedReader(new StringReader(st));
+	  //String[] lines = st.split(System.getProperty("line.separator"));
+	  //System.out.println("bbbbbbbb " + lines);
+	  String line=null;
+	  try {
+		while( (line=bufReader.readLine()) != null )
+		  {
+			s1 = new Subscriber(null,null,null,null,null,null,null);
+			  String[] result = line.split("\\s");
+			  s1.setFirstname(result[0]);
+			  s1.setLastname(result[1]);
+			  s1.setId(result[2]);
+			  s1.setPhonenumber(result[3]);
+			  s1.setEmailaddress(result[4]);
+			  s1.setCreditcardnumber(result[5]);
+			  s1.setSubscribernumber(result[6]);
+			  subs.add(s1);
+		  }
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	 // for (String line : lines) {
+
+	 // }
+  }
+  
+  public void handleMessageFromServer(String msg) 
+  {
+	  System.out.println("--> handleMessageFromServer");
+	  System.out.println(msg);
+	  servermsg = msg.toString();
+	  awaitResponse = false;
+  }
+
+  /**
+   * This method handles all data coming from the UI            
+   *
+   * @param message The message from the UI.    
+   */
+    public void handleMessageFromClientUI(String message)
+  {
+    try
+    {
+    	openConnection();//in order to send more than one message
+       	awaitResponse = false;
+        Message msg = new Message(message, MessageFromClient.LOGIN_REQUEST); // TODO: change the login to a more generic one...
+
+        try{
+            sendToServer(msg);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+		// wait for response
+		while (awaitResponse) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+    }
+    catch(IOException e)
+    {
+    	e.printStackTrace();
+      quit();
+    }
+  }
+  /**
+   * This method terminates the client.
+   */
+  public void quit()
+  {
+    try
+    {
+      closeConnection();
+    }
+    catch(IOException e) {}
+    System.exit(0);
+  }
+}
+//End of ChatClient class
+
+
---
+++ Client/src/gui/SubscriberEditController.java
@@ -0,0 +1,155 @@
+package gui;
+
+import Application.Common.Subscriber;
+import Application.client.ChatClient;
+import Application.client.ClientUI;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SubscriberEditController implements Initializable {
+		
+	ObservableList<String> list;
+	double xoffset;
+	double yoffset;
+	@FXML
+	private Label lblName;
+	@FXML
+	private Label lblSurname;
+	@FXML
+	private Label lblselect;
+	@FXML
+	private TableColumn<Subscriber, String> txtFirstname;
+	@FXML
+	private TableColumn<Subscriber, String>   txtLastname;
+	@FXML
+	private TableColumn<Subscriber, String>   txtId;
+	@FXML
+	private TableColumn<Subscriber, String>   txtPhonenumber;
+	@FXML
+	private TableColumn<Subscriber, String>   txtEmailaddress;
+	@FXML
+	private TableColumn<Subscriber, String>  txtCreditcardnumber;
+	@FXML
+	private TableColumn<Subscriber, String>  txtSubscribernumber;
+	@FXML
+	private Button btnclose=null;
+	@FXML
+	private Button btnUpdate=null;
+	@FXML
+	private Button btnRefresh=null;
+	@FXML
+	 private TableView<Subscriber> Table;
+	private ObservableList<Subscriber> observablesubs = FXCollections.observableArrayList();
+
+	public void loadSubscriber() {
+		ClientUI.chat.accept("login");
+		Table.getItems().clear();
+		
+        for(Subscriber s : ChatClient.subs){
+            if (s.toString().equals("null"))
+                continue;
+            Subscriber subsData = new Subscriber("", "" ,"","","","","");
+            subsData.setFirstname(s.getFirstname());
+            subsData.setLastname(s.getLastname());
+            subsData.setId(s.getId());
+            subsData.setPhonenumber(s.getPhonenumber());
+            subsData.setEmailaddress(s.getEmailaddress());
+            subsData.setCreditcardnumber(s.getCreditcardnumber());
+            subsData.setSubscribernumber(s.getSubscribernumber());
+            Table.getItems().add(subsData);
+        }
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		System.out.println();
+		txtFirstname.setCellValueFactory(new PropertyValueFactory<>("Firstname"));
+		txtLastname.setCellValueFactory(new PropertyValueFactory<>("Lastname"));
+		txtId.setCellValueFactory(new PropertyValueFactory<>("Id"));
+		txtPhonenumber.setCellValueFactory(new PropertyValueFactory<>("Phonenumber"));
+		txtEmailaddress.setCellValueFactory(new PropertyValueFactory<>("Emailaddress"));
+		txtCreditcardnumber.setCellValueFactory(new PropertyValueFactory<>("Creditcardnumber"));
+		txtSubscribernumber.setCellValueFactory(new PropertyValueFactory<>("Subscribernumber"));
+		Table.setItems(observablesubs);
+		loadSubscriber();
+		
+	}
+	
+	public void Closebtn(ActionEvent event) throws Exception {
+		ClientUI.chat.accept("disconnect");
+        ((Node)event.getSource()).getScene().getWindow().hide();
+        
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/ClientConnector.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/gui/ClientConnector.css").toExternalForm());
+        root.setOnMousePressed(event1 -> {
+            xoffset = event1.getSceneX();
+            yoffset = event1.getSceneY();
+        });
+
+        // event handler for when the mouse is pressed AND dragged to move the window
+        root.setOnMouseDragged(event1 -> {
+            primaryStage.setX(event1.getScreenX()-xoffset);
+            primaryStage.setY(event1.getScreenY()-yoffset);
+        });
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Academic Managment Tool");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+	
+	public void Updatebtn(ActionEvent event) throws Exception {
+        if (Table.getSelectionModel().getSelectedItem() != null) {
+            Subscriber selectedsub = Table.getSelectionModel().getSelectedItem();
+            Stage primaryStage = new Stage();
+            
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/SubscriberSave.fxml"));
+            
+            Parent root = loader.<Parent>load();
+            Scene scene = new Scene(root);
+            SubscriberSaveController subscribersavecontroller = loader.<SubscriberSaveController>getController();
+            subscribersavecontroller.loadSubscriber(selectedsub);
+            scene.getStylesheets().add(getClass().getResource("/gui/SubscriberSave.css").toExternalForm());
+            root.setOnMousePressed(event1 -> {
+                xoffset = event1.getSceneX();
+                yoffset = event1.getSceneY();
+            });
+
+            // event handler for when the mouse is pressed AND dragged to move the window
+            root.setOnMouseDragged(event1 -> {
+                primaryStage.setX(event1.getScreenX()-xoffset);
+                primaryStage.setY(event1.getScreenY()-yoffset);
+            });
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.setTitle("Save Subscriber Details");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.initModality(Modality.APPLICATION_MODAL);
+            primaryStage.showAndWait();
+            lblselect.setVisible(false);
+		 } 
+        else {
+        	lblselect.setVisible(true);
+        }
+	}
+	
+}
